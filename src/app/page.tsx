6b115417432e685036fced10688fb49294726fd7
Matheus Marchetti <matheusmarchetti1502@gmail.com>
2024-11-05 11:05:43 -0300

    tecnologias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <body>
       <div className="h-full bg-slate-600">
-        <header className="flex px-56 py-5">
+        <header className="flex px-56 h-20 py-2 ">
           <div className="bg-red-500 w-1/4 flex justify-center items-center">
             <img
               src="https://i.seadn.io/gae/RC64Qbc-vI5edW8LsHK-LZ0rvMTmCWBepr3uti5Uzq2n3eCiQnaXqpgfLz8WvNRLOyM6MKRX-HTdyB939AVoBm4Rc92HrkGVKZ8G?auto=format&dpr=1&w=1000"
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="bg-yellow-300 flex-1 flex justify-center items-center">
             <nav className="flex space-x-8 bg-blue-400">
               <a href="#sobre">Sobre</a>
-              <a href="">Tecnologia</a>
+              <a href="#tecnologia">Tecnologia</a>
               <a href="">Equipe</a>
               <a href="">Contato</a>
               <a href="">Atualizações</a>
@@ -52,6 +52,22 @@ export default function Home() {
               alt=""
               className="h-5/6"
             />
+          </div>
+        </section>
+        <section
+          id="tecnologia"
+          className="h-screen bg-green-500 px-56 py-5 flex justify-center space-x-4"
+        >
+          <div className="bg-purple-500 w-full h-full py-4 items-center flex flex-col space-y-2 ">
+            <h1>Tecnologias</h1>
+            <div className="bg-red-500 w-full h-full grid grid-cols-3 gap-2 p-2">
+            <div className="bg-blue-700 p-4 text-center">ReactNative</div> 
+            <div className="bg-blue-500 p-4 text-center">React</div> 
+            <div className="bg-orange-500 p-4 text-center">JavaScript</div> 
+            <div className="bg-blue-300 p-4 text-center">TypeScript</div> 
+            <div className="bg-blue-500 p-4 text-center">TailWind CSS</div> 
+            <div className="bg-gray-500 p-4 text-center">Expo</div>
+            </div>
           </div>
         </section>
       </div>
